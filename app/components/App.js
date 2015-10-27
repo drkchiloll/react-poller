feb9d2425402cc8150b978f0fcdb05aefc91e884
drkchiloll <samwomack_@me.com>
2015-10-27 12:54:41 -0500

    Wrote Client Side Socket Connection Disconnection methods

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Component from './Component';
+import Header from './Header';
+import IO from 'socket.io-client';
 
 export default class App extends React.Component {
+  state = {
+    status : 'disconnected'
+  }
+  constructor() {
+    super();
+    this.socket = IO('http://localhost:3000');
+    this.connect = this.connect.bind(this);
+  }
+  componentWillMount() {
+    this.socket.on('connect', this.connect);
+    this.socket.on('disconnect', this.disconnect);
+  }
+  connect() {
+    this.setState({ status : 'connected' });
+  }
+  disconnect() {
+    this.setState({ status : 'disconnected'});
+  }
   render() {
     return (
-      <Component />
+      <Header title='New Header' status={this.state.status}/>
     );
   }
 }
